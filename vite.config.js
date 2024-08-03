83e816ea939731e5a2bcc4594f938ac85e6223a6
sachin nagar <sachinnagar@sachins-MacBook-Air.local>
2024-08-03 08:10:18 +0530

    last commit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port:process.env.PORT || 50505050,
+    port:process.env.PORT || 62321,
   },
 })

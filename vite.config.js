5159bdea1d3f49e39c98e29f5924086eca42b88e
sachin nagar <sachinnagar@sachins-MacBook-Air.local>
2024-08-11 18:50:07 +0530

    recommit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: Number(import.meta.env.VITE_PORT) || 5000,
-  },
-})
+
+    
+    export default defineConfig({
+      plugins: [react()],
+      server: {
+        port:process.env.PORT||5000,
+      },
+    })
+    

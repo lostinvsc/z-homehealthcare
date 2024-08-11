feaf7d8e82bdbad28f5d6c363a4953af397d3226
sachin nagar <sachinnagar@sachins-MacBook-Air.local>
2024-08-12 00:07:28 +0530

    6

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
-dotenv.config()
 
-const port = Number(process.env.PORT) || 3000;
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: port, 
-  },
-  
 })
